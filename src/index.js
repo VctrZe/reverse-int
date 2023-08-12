637b79b578fcc2af7f560e84fd4a4436c63f3e8c
Viktoriia Zelenska <vikazgit@gmail.com>
2023-08-11 22:01:10 -0700

    feat: implement reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
-module.exports = function reverse (n) {
+function reverse(n) {
+  let nun = Math.abs(n);
+  const str = nun.toString().split('');
+  const length = str.length;
+  let reversed = [];
+  let result;
   
+  for (let i = 0; i <= length; i++) {         // 678439
+    
+    reversed[i] = str[(length - i)];
+    
+    console.log(reversed)
+    result = reversed.join('');
+  }
+
+  return result;
 }
+
+console.log(reverse(-674052))
+
+module.exports = reverse;
