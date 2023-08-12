0b77ceecbbfcdcbbbb6b00b6385ffe4a51039596
Viktoriia Zelenska <vikazgit@gmail.com>
2023-08-11 22:46:14 -0700

    fix: change reverse function

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 module.exports = function reverse(n) {
-  let nun = Math.abs(n);
-  const str = nun.toString().split('');
-  const length = str.length;
-  let reversed = [];
-  let result;
-  
-  for (let i = 0; i <= length; i++) {         // 678439
-    
-    reversed[i] = str[(length - i)];
-    
-    console.log(reversed)
-    result = reversed.join('');
+  const num = Math.abs(n);
+  const arr = num.toString().split('');
+  let tmp
+  for (let i = 0; i < arr.length / 2; i++) {         // 678439
+    tmp = arr[i]
+    arr[i] = arr[arr.length - 1 - i]
+    arr[arr.length - 1 - i] = tmp
   }
 
-  return result;
+  return Number(arr.join(''));
 }
